e408db7d16511c02f4589f15421df8c203c66a63
Lakshmi Devarapalli <lakshmi.devarapalli@acutest.com>
2019-08-14 23:02:47 +0100

    First winium test commit in github

---
+++ src/test/java/features/windows-test.feature
@@ -0,0 +1,6 @@
+Feature: Notepad plus
+
+  Scenario: Open Notepad plus and save the document
+    Given user opened the notepad plus
+    When user entered some random text
+    Then user saved the notepad file
---
+++ src/test/java/steps/MyStepdefs.java
@@ -0,0 +1,56 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.winium.DesktopOptions;
+import org.openqa.selenium.winium.WiniumDriver;
+import org.openqa.selenium.winium.WiniumDriverService;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.io.File;
+
+public class MyStepdefs {
+
+    private WebDriver driver = null;
+
+    @Given("^user opened the notepad plus$")
+    public void userOpenedTheNotepadPlus() throws InterruptedException {
+
+        String notepadAppPath = "C:\\Program Files (x86)\\Notepad++\\notepad++.exe";
+        String winiumDriverPath = "C:\\Users\\KODEV\\IdeaProjects\\winiumTestProject\\Winium.Desktop.Driver.exe";
+        DesktopOptions options = new DesktopOptions();
+        options.setApplicationPath(notepadAppPath);
+        WiniumDriverService service = new WiniumDriverService.Builder().usingDriverExecutable
+                (new File(winiumDriverPath)).usingPort(9999).withVerbose(true).withSilent(false).buildDesktopService();
+        Thread.sleep(5000);
+        driver = new WiniumDriver(service, options);
+        Thread.sleep(10000);
+    }
+
+    @When("^user entered some random text$")
+    public void userEnteredSomeRandomText() {
+        driver.findElement(By.id("Item 41001")).click();
+        driver.findElement(By.className("Scintilla")).sendKeys("This is an sample winium Test");
+        driver.findElement(By.id("Item 41006")).click();
+
+    }
+
+    @Then("^user saved the notepad file$")
+    public void userSavedTheNotepadFile() throws AWTException {
+        Robot robo = new Robot();
+        robo.mouseMove(82,191);
+        robo.delay(2000);
+        robo.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robo.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        driver.findElement(By.id("1001")).sendKeys("testingFile");
+        driver.findElement(By.name("Save")).click();
+        driver.close();
+        driver.quit();
+    }
+
+
+}
